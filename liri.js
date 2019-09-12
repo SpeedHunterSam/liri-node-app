8330fc2d274f062911dcf0e4a14b14b267d87e96
Brian <brian.s2@gmail.com>
2019-09-11 21:19:21 -0500

    added spotify and devenv functionality

--- liri.js
+++ liri.js
@@ -1,2 +1,25 @@
 require("dotenv").config();
 const keys = require("./keys.js");
+const Spotify = require('node-spotify-api');
+
+ 
+let spotify = new Spotify(keys.spotify);
+ 
+
+
+function startLiri(){
+
+    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log(data); 
+      });
+
+      
+
+}
+
+startLiri();
+
